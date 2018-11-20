05a9c07c88f5e0ef5e0419df77c473b932d9fc97
Florian Bar <Florianb@payspace.com>
2018-11-20 09:35:14 +0200

    Added model and scafolding

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -34,6 +34,7 @@ namespace MvcMovie.Controllers
 
             var movie = await _context.Movie
                 .FirstOrDefaultAsync(m => m.ID == id);
+
             if (movie == null)
             {
                 return NotFound();
